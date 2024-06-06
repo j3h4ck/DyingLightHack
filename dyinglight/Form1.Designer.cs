d9c5525f90bb6db6f29b5fba2f3a594fdf355671
j3h4ck <107075824+j3h4ck@users.noreply.github.com>
2024-06-06 22:34:26 +0300

    Add files via upload

---
+++ dyinglight/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace dyinglight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flyhack = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.zcalc = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(571, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(250, 280);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(624, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start cheat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flyhack
+            // 
+            this.flyhack.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fly Hack";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(156, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "activate";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // zcalc
+            // 
+            this.zcalc.Enabled = true;
+            this.zcalc.Interval = 1000;
+            this.zcalc.Tick += new System.EventHandler(this.zcalc_Tick);
+            // 
+            // label2
+            // 
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(369, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(369, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Y : ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(369, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Z :";
+            // 
+            // label5
+            // 
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(341, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "F10";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 185);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 172);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(92, 360);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(230, 26);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Dying Light 1 Enhanced Edition multihack v1.0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(92, 386);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(230, 26);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "made by @j3h4ck";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(48, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "base";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Lime;
+            this.button4.Location = new System.Drawing.Point(177, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "lake";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Lime;
+            this.button5.Location = new System.Drawing.Point(310, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "bridge";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label8
+            // 
+            this.label8.ForeColor = System.Drawing.Color.Lime;
+            this.label8.Location = new System.Drawing.Point(169, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(138, 26);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Teleport To    ";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(872, 428);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Dying Light Enhanced Edition multihack";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer flyhack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer zcalc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ dyinglight/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace dyinglight
+{
+    public partial class Form1 : Form
+    {
+        Mem m = new Mem();
+        int PID;
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+        string x_address = "engine_x64_rwdi.dll+00A8E2E8,280,0,700,BC";
+        string y_address = "engine_x64_rwdi.dll+00A8E2E8,280,0,700,C4";
+        string z_address = "engine_x64_rwdi.dll+00A8E2E8,280,0,700,C0";
+        float z;
+        float x;
+        float y;
+        float z2;
+        bool fly1 = false;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.TopMost = true;
+            
+            button1.ForeColor = Color.Red;
+            button2.ForeColor = Color.Red;
+
+
+        }
+        public void Cheat()
+        {
+            
+            z = m.ReadFloat(z_address);
+            //MessageBox.Show("z is : " + z);
+            //backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PID = m.GetProcIdFromName("DyingLightGame.exe");
+            if (PID == 0)
+            {
+                richTextBox1.AppendText("[-] Process isnt running\n");
+            }
+            else
+            {
+                button1.ForeColor = Color.LimeGreen;
+                richTextBox1.AppendText("[+] Game is running\n");
+                richTextBox1.AppendText("[+] PID : " + PID + "\n");
+                m.OpenProcess(PID);
+                Cheat();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Thread.Sleep(10);
+            if (GetAsyncKeyState(Keys.Space) < 0)
+            {
+                z = m.ReadFloat(z_address);
+                z2 = z + 8;
+                
+                //richTextBox1.AppendText("old z : " + z + "\n");
+                //label1.Text += "old z : " + z + "\n";
+                //label1.Text += "new z : " + z2 + "\n";
+                //richTextBox1.AppendText("new z : " + z2 + "\n");
+                m.WriteMemory(z_address,"float", z2.ToString());
+                Thread.Sleep(10);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fly1 = !fly1;
+            if (fly1 == false)
+            {
+                button2.ForeColor = Color.Red;
+                button2.Text = "deactivated";
+                flyhack.Stop();
+                richTextBox1.AppendText("[-] Fly Hack Deactivated\n");
+            }
+            else if (fly1 == true)
+            {
+                button2.ForeColor = Color.LimeGreen;
+                button2.Text = "activated";
+                flyhack.Start();
+                richTextBox1.AppendText("[+] Fly Hack Activated\n");
+            }
+
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zcalc_Tick(object sender, EventArgs e)
+        {
+            x = m.ReadFloat(x_address);
+            y = m.ReadFloat(y_address);
+            z = m.ReadFloat(z_address);
+            label2.Text = "x : " + x.ToString();
+            label3.Text = "z : " + z.ToString();
+            label4.Text = "y : " + y.ToString();
+            if (GetAsyncKeyState(Keys.F10) < 0)
+            {
+                fly1 = !fly1;
+                if (fly1 == false)
+                {
+                    button2.ForeColor = Color.Red;
+                    button2.Text = "deactivated";
+                    flyhack.Stop();
+                    richTextBox1.AppendText("[-] Fly Hack Deactivated\n");
+                }
+                else if (fly1 == true)
+                {
+                    button2.ForeColor = Color.LimeGreen;
+                    button2.Text = "activated";
+                    flyhack.Start();
+                    richTextBox1.AppendText("[+] Fly Hack Activated\n");
+                }
+            }
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float xbase = 272.5458984f;
+            float ybase = 88.02549744f;
+            float zbase = 134.3164368f;
+            m.WriteMemory(x_address, "float", xbase.ToString());
+            m.WriteMemory(y_address, "float", ybase.ToString());
+            m.WriteMemory(z_address, "float", zbase.ToString());
+            richTextBox1.AppendText("[*] Teleporting to base\n");
+            richTextBox1.AppendText("[+] Teleported successfully\n");
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float xlake = 0.2735294104f;
+            float ylake = 73.82168579f;
+            float zlake = 98.27256775f;
+            m.WriteMemory(x_address, "float", xlake.ToString());
+            m.WriteMemory(y_address, "float", ylake.ToString());
+            m.WriteMemory(z_address, "float", zlake.ToString());
+            richTextBox1.AppendText("[*] Teleporting to lake\n");
+            richTextBox1.AppendText("[+] Teleported successfully\n");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            float xbridge = -132.0366669f;
+            float ybridge = 278.3412781f;
+            float zbridge = 237.6899109f;
+            m.WriteMemory(x_address, "float", xbridge.ToString());
+            m.WriteMemory(y_address, "float", ybridge.ToString());
+            m.WriteMemory(z_address, "float", zbridge.ToString());
+            richTextBox1.AppendText("[*] Teleporting to bridge\n");
+            richTextBox1.AppendText("[+] Teleported successfully\n");
+        }
+    }
+}
